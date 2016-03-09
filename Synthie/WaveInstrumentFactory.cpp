80efd976e31c6688547e904508088ad530e426e3
Wyatt Roehler <roehlerwya@gmail.com>
2016-03-08 21:09:27 -0500

    All features of Wavetable added

--- Synthie/WaveInstrumentFactory.cpp
+++ Synthie/WaveInstrumentFactory.cpp
@@ -2,12 +2,13 @@
 #include "WaveInstrumentFactory.h"
 #include <cmath>
 
+using namespace std;
+
 CWaveInstrumentFactory::CWaveInstrumentFactory(void)
 {
-	for (double time = 0; time<2; time += 1. / 44100.)
-	{
-		m_wave.push_back(short(3267 * sin(2 * 3.1415 * 1000 * time)));
-	}
+	m_fade = 0.5;
+	m_attack = 0.05;
+	m_release = 0.05;
 }
 
 CWaveInstrumentFactory::~CWaveInstrumentFactory(void)
@@ -51,27 +52,48 @@ void CWaveInstrumentFactory::SetNote(CNote *note)
 		CComVariant value;
 		attrib->get_nodeValue(&value);
 
-		if (name == "duration")
-		{
-		}
-
-		else if (name == "file")
+		if (name == "file")
 		{
 			value.ChangeType(VT_BSTR);
 			char filename[100];
-			wcstombs(filename, value.bstrVal,100);
+			wcstombs(filename, value.bstrVal, 100);
 			LoadFile(filename);
+		}
+		else if (name == "duration")
+		{
+			value.ChangeType(VT_R8);
+			SetDuration(value.dblVal);
+		}
+		else if (name == "attack")
+		{
+			value.ChangeType(VT_R8);
+			SetAttack(value.dblVal);
+		}
+		else if (name == "release")
+		{
+			value.ChangeType(VT_R8);
+			SetRelease(value.dblVal);
 		}
 		else if (name == "pitch")
 		{
-			//ChangePitch();
+			value.ChangeType(VT_R8);
+			ChangePitch(value.dblVal);
 		}
-		else if (name == "glissando")
+		else if (name == "fade")
 		{
-			//Glissando();
+			value.ChangeType(VT_R8);
+			SetFade(value.dblVal);
+		}
+		else if (name == "crossfade")
+		{
+			value.ChangeType(VT_BSTR);
+			char filename[100];
+			wcstombs(filename, value.bstrVal, 100);
+			CrossFade(filename);
 		}
 	}
 
+	Envelope();
 }
 
 
@@ -102,14 +124,110 @@ bool CWaveInstrumentFactory::LoadFile(const char *filename)
 void CWaveInstrumentFactory::ChangePitch(double rate)
 {
 	double sample = 0.0;
+	vector<short> p_wave;
 
-	double	inbetween = fmod(sample, 1);
-	short output = (1. - inbetween) * m_wave[int(sample)] + inbetween * m_wave[int(sample) + 1];
+	if (rate == 1.0)
+	{
+		return;
+	}
 
-	sample += rate;
+	while (sample < m_wave.size()-1)
+	{
+		double	inbetween = fmod(sample, 1);
+		short output = (1. - inbetween) * m_wave[int(sample)] + inbetween * m_wave[int(sample) + 1];
+
+		p_wave.push_back(output);
+
+		sample += rate;
+	}
+
+	m_wave = p_wave;
 }
 
-void CWaveInstrumentFactory::Glissando(double rate)
+void CWaveInstrumentFactory::CrossFade(const char *filename)
 {
+	vector<short> f_wave;
 
+	CDirSoundSource f_file;
+	if (!f_file.Open(filename))
+	{
+		CString msg = L"Unable to open audio file: ";
+		msg += filename;
+		AfxMessageBox(msg);
+		return;
+	}
+
+	for (int i = 0; i<f_file.NumSampleFrames(); i++)
+	{
+		short frame[2];
+		f_file.ReadFrame(frame);
+		f_wave.push_back(frame[0]);
+	}
+
+	f_file.Close();
+
+	double output;
+	int m_sample = 0;
+	int o_sample = 0;
+	double m_duration = m_wave.size()/44100.;
+	double fade_start = m_duration - m_fade;
+	int f_sample = 0;
+	double time = 0.0;
+	vector<short> o_wave;
+
+	while (f_sample < f_wave.size())
+	{
+		if (time < fade_start)
+		{
+			o_wave.push_back(m_wave[m_sample]);
+			m_sample++;
+			o_sample++;
+			time += 1 / 44100.;
+		}
+		else if (time < (fade_start + m_fade))
+		{
+			if (m_sample > m_wave.size() - 1)
+			{
+				m_sample = m_wave.size() - 1;
+			}
+			output = (1 - ((time - fade_start) /m_fade))*m_wave[m_sample] + ((time - fade_start) / m_fade)*f_wave[f_sample];
+			o_wave.push_back(short(output));
+
+			m_sample++;
+			o_sample++;
+			f_sample++;
+			time += 1 / 44100.;
+		}
+		else
+		{
+			o_wave.push_back(f_wave[f_sample]);
+			o_sample++;
+			f_sample++;
+		}
+	}
+
+	m_wave = o_wave;
+}
+
+void CWaveInstrumentFactory::Envelope()
+{
+	double output;
+	double m_ramp;
+	double m_time = 0.0;
+	double m_duration = m_wave.size() / 44100.;
+
+	for (int i = 0; i < m_wave.size(); i++, m_time += 1 / 44100)
+	{
+		if (m_time < m_attack)
+		{
+			m_ramp = (m_time / m_attack);
+		}
+		if (m_time >(m_duration - m_release))
+		{
+			m_ramp = (m_duration - m_time) / m_release;
+		}
+
+		output = m_wave[i] * m_ramp;
+		m_wave[i] = short(output);
+	}
 }
--- Synthie/WaveInstrumentFactory.h
+++ Synthie/WaveInstrumentFactory.h
@@ -13,9 +13,19 @@ public:
 	void SetNote(CNote *note);
 	bool LoadFile(const char *filename);
 	void ChangePitch(double rate);
-	void Glissando(double rate);
+	void CrossFade(const char *filename);
+	void CWaveInstrumentFactory::Envelope();
+
+	void SetFade(double fade) { m_fade = fade; }
+	void SetAttack(double attack) { m_attack = attack; }
+	void SetRelease(double release) { m_release = release; }
+	void SetDuration(double duration) { m_duration = duration; }
 
 private:
 	std::vector<short> m_wave;
+	double m_fade;
+	double m_attack;
+	double m_release;
+	double m_duration;
 };
 

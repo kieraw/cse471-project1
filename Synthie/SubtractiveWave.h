8ba197b3c8dcf113f7f16a9f2ae618904963da8d
Eddy Maxwell <maxwe117@msu.edu>
2016-03-15 17:57:03 -0400

    subtr changes

--- Synthie/SubtractiveWave.h
+++ Synthie/SubtractiveWave.h
@@ -27,9 +27,9 @@ private:
 	double m_time;
 	//Apply Reson?
 	bool m_implement_reson = false;
-	//emphasized reson frequency, has default value
+	//emphasized reson frequency, range of 0-0.5, has default value
 	double m_reson_freq = 0.01134;
-	//emphasized reson bandwidth, has default value
+	//emphasized reson bandwidth, range of 0-1, has default value
 	double m_reson_bandwidth = 0.01;
 };
 
